49f2ad3fc132ce2fb79f943f5a92b24cd6630adc
GNataliya <Nataliya.Graff@gmail.com>
2021-04-23 22:55:40 +0300

    level 3

--- js/main.js
+++ js/main.js
@@ -1,111 +1,115 @@
-let ElInputNum1 = document.querySelector('.num1');
-let ElInputNum2 = document.querySelector('.num2');
+// let ElInputNum1 = document.querySelector('.num1');
+// let ElInputNum2 = document.querySelector('.num2');
 let getResult = document.querySelector('.result');
 let btnMultiply = document.querySelector('.btn-multiply');
 let btnDivide = document.querySelector('.btn-divide');
 let btnPow = document.querySelector('.btn-pow');
 let btnRoot = document.querySelector('.btn-root');
 
+
+let getValue = () => {
+    const form = document.forms.numbers;
+
+    const data = new FormData(form);
+
+    const num1 = Number(data.get('num1').trim());
+    const num2 = Number(data.get('num2').trim());
+    const num3 = Number(data.get('num3').trim());
+
+    return [num1, num2, num3];
+}
+
+let putResult = (value) => {
+    const val = getValue();
+    let getResult = document.querySelector('.result');
+    getResult.innerHTML = value.toFixed(val[2]);
+}
+
 btnMultiply.addEventListener('click', (ev) => {
-
-    let num1 = Number(ElInputNum1.value);
-    let num2 = Number(ElInputNum2.value);
-
-    let multiply = () => {
-        return (num1 * num2);
-    };
-    getResult.innerHTML = multiply();
-});
-
-
+    const val = getValue();
+    const result = val[0] * val[1];
+    putResult(result);
+    });
+    
+    
 btnDivide.addEventListener('click', (ev) => {
-
-    let num1 = Number(ElInputNum1.value);
-    let num2 = Number(ElInputNum2.value);
+    const val = getValue();
+    const result = val[0] / val[1];
+    putResult(result);
+    });
     
-    let divide = () => {
-        return (num1 / num2);
-    };
-    getResult.innerHTML = divide();
-});
-
-
+    
 btnPow.addEventListener('click', (ev) => {
-
-    let num1 = Number(ElInputNum1.value);
-    let num2 = Number(ElInputNum2.value);
+    const val = getValue();
+    const result = val[0] ** val[1];
+    putResult(result);
+    });
     
-    // let power = () => {
-    //     let pow = Math.pow(num1, num2);
-    //     return (pow);
-    // };
-
-    let power = () => {
-        return (num1 ** num2);
-    };
-    getResult.innerHTML = power();
-});
-
-
+    
 btnRoot.addEventListener('click', (ev) => {
+    const val = getValue();
+    const result = Math.pow(val[0], (1/val[1]));
+    putResult(result);
+    });
 
-    let num1 = Number(ElInputNum1.value);
-    let num2 = Number(ElInputNum2.value);
-    
-    let root = () => {
-        return (Math.pow(num1, (1/num2)));
-    };
-
-    // let root = () => {
-    //     return (num1 ** (1/num2));
-    // };
-    getResult.innerHTML = root();
-});
 
 
 
 // Solution 2:
 
-// let getValue = () => {
-//     const form = document.forms.numbers;
-
-//     const data = new FormData(form);
-
-//     const num1 = Number(data.get('num1').trim());
-//     const num2 = Number(data.get('num2').trim());
-
-//     return [num1, num2];
-// }
-
-// let putResult = (value) => {
-//     let getResult = document.querySelector('.result');
-//     getResult.innerHTML = value;
-// }
-
 // btnMultiply.addEventListener('click', (ev) => {
-//     const val = getValue();
-//     const result = val[0] * val[1];
-//     putResult(result);
-//     });
-    
-    
+
+//     let num1 = Number(ElInputNum1.value);
+//     let num2 = Number(ElInputNum2.value);
+
+//     let multiply = () => {
+//         return (num1 * num2);
+//     };
+//     getResult.innerHTML = multiply();
+// });
+
+
 // btnDivide.addEventListener('click', (ev) => {
-//     const val = getValue();
-//     const result = val[0] / val[1];
-//     putResult(result);
-//     });
+
+//     let num1 = Number(ElInputNum1.value);
+//     let num2 = Number(ElInputNum2.value);
     
-    
+//     let divide = () => {
+//         return (num1 / num2);
+//     };
+//     getResult.innerHTML = divide();
+// });
+
+
 // btnPow.addEventListener('click', (ev) => {
-//     const val = getValue();
-//     const result = val[0] ** val[1];
-//     putResult(result);
-//     });
+
+//     let num1 = Number(ElInputNum1.value);
+//     let num2 = Number(ElInputNum2.value);
     
-    
+//     // let power = () => {
+//     //     let pow = Math.pow(num1, num2);
+//     //     return (pow);
+//     // };
+
+//     let power = () => {
+//         return (num1 ** num2);
+//     };
+//     getResult.innerHTML = power();
+// });
+
+
 // btnRoot.addEventListener('click', (ev) => {
-//     const val = getValue();
-//     const result = Math.pow(val[0], (1/val[1]));
-//     putResult(result);
-//     });
+
+//     let num1 = Number(ElInputNum1.value);
+//     let num2 = Number(ElInputNum2.value);
+    
+//     let root = () => {
+//         return (Math.pow(num1, (1/num2)));
+//     };
+
+//     // let root = () => {
+//     //     return (num1 ** (1/num2));
+//     // };
+//     getResult.innerHTML = root();
+// });
 
